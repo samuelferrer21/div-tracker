468b9cf53c7e6fd408e95ff2258fef9f322771d7
sam <samferrer145@gmail.com>
2024-04-15 11:43:58 -0500

    Added Navigation
    Navigation between root and dashboard are working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -8,4 +9,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  #Dashboard route
+  get "dashboard", to: "dashboard#index"
 end
