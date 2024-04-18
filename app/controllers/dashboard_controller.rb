715b0971cc35fc42b5db7ca8d0717693f8f3a087
sam <samferrer145@gmail.com>
2024-04-17 19:45:10 -0500

    Added authorization

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,19 @@
 class DashboardController < ApplicationController
   def index
-
     # Fetches the query
     if params[:query].present?
       flash.now[:message] = "Showing results for Ticker containing '#{params[:query]}'."
+
+      response = Faraday.get('https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=RfeXt38_qV89hpNGl4UIfd6NRzC4PgUP0')
+      logger.info(response)
+
+
+      #response = Faraday.get "https://api07.iq.questrade.com/v1/symbols/search?prefix=#{params[:query]}", params do |request|
+        #request.headers["Authorization"] = "Bearer KxSIVYI8r6hjaS39xfQExLx7ztyd6hae0"
+      #end
+      #logger.info(response)
+      #puts response.body[]
+
     end
   end
 end
