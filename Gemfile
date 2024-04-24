b1b44485cb729d5e9ebd6fd5fa9a27af872ba169
sam <samferrer145@gmail.com>
2024-04-24 15:51:04 -0500

    Devise working
    Devise has functioning login and registration through modals.

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ group :test do
 end
 
 gem "tailwindcss-rails", "~> 2.3"
+#Devise gem
+gem 'devise'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+
+  helper_method :user_signed_in?
+
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
