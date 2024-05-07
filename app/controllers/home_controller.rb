58c23412a877959bae480337ce734e6f5fade2ba
sam <samferrer145@gmail.com>
2024-05-06 19:58:06 -0500

    Security update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,33 @@
 class HomeController < ApplicationController
   def index
    @UserIsSignedIn= user_signed_in?
-   logger.info(@UserIsSignedIn)
 
    #Retrieves the authorization code from Questrade
    if @UserIsSignedIn & !params[:code].nil?
      #Sends a post request once code is recieved to get access and refresh token
      response = Faraday.get("https://login.questrade.com/oauth2/token?client_id=YTGdFwCpGjB76WxOJAmmBOem0n1QvA&code=#{params[:code]}&grant_type=authorization_code&redirect_uri=https://div-tracker-tjzx7q7eeq-uc.a.run.app")
-     puts JSON.parse(response.body)
+     #Parses the response to json
+     parsedResponse = JSON.parse(response.body)
 
-    #Insert the tokens to db
-    @token_new = Token.new()
-    @token_new.access_token = JSON.parse(response.body)["access_token"]
-    @token_new.refresh_token = JSON.parse(response.body)["refresh_token"]
-    @token_new.save
+     logger.info(parsedResponse['access_token'])
+     logger.info(parsedResponse['refresh_token'])
 
-    #View Token
-    @token_view = Token.all
-    logger.info(@token_view.inspect)
+    if Token.find_by(user_id: current_user.id).nil?
+      #Insert the tokens to db if no current token exists
+      token_new = Token.new(access_token: parsedResponse['access_token'], refresh_token: parsedResponse['refresh_token'], user_id: current_user.id)
+      token_new.save
+      #Save to session and we update it by checking the session and replacing the token
+      session[:access_token] = parsedResponse['access_token']
+      session[:refresh_token] = parsedResponse['refresh_token']
+    else
+      #Replaces the current token with the new one
+      token_new = Token.find_by(user_id: current_user.id).update(access_token: parsedResponse['access_token'], refresh_token: parsedResponse['refresh_token'], last_updated: Time.now, user_id: current_user.id)
+      token_new.save
 
+      session[:access_token] = parsedResponse['access_token']
+      session[:refresh_token] = parsedResponse['refresh_token']
+      #Save to session and we update it by checking the session and replacing the token
+    end
    end
   end
 end
