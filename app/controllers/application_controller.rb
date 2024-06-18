f9d7aaf5a76ab35a426eda7e3865844e858d9339
sam <samferrer145@gmail.com>
2024-06-18 17:13:59 -0500

    Fixed expired Tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,21 +27,32 @@ class ApplicationController < ActionController::Base
     if !Token.find_by(user_id: current_user.id).nil?
       session[:last_updated] = Token.find_by(user_id: current_user.id).last_updated
 
-
       #updates the access and refresh token if the difference between current time and last updated is more than 30 minutes.
       if Time.now - Token.find_by(user_id: current_user.id).last_updated > 1800
-        puts "Token being updated"
-        #Sends a post request once code is recieved to get access and refresh token
-        response = Faraday.get("https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=#{Token.find_by(user_id: current_user.id).refresh_token}")
-        #Parses the response to json
-        puts JSON.parse(response.body)
+        #Tries to update the token if the token is invalid destroys the old token
+        begin
+          puts "Token being updated"
 
-        parsedResponse = JSON.parse(response.body)
+          #Sends a post request once code is recieved to get access and refresh token
+          response = Faraday.get("https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=#{Token.find_by(user_id: current_user.id).refresh_token}")
+          #Parses the response to json
+          puts JSON.parse(response.body)
 
-        #Updates db and session to new tokens
-        token_new = Token.find_by(user_id: current_user.id).update(access_token: parsedResponse['access_token'], refresh_token: parsedResponse['refresh_token'], api_server: parsedResponse['api_server'], last_updated: Time.now, user_id: current_user.id)
+          parsedResponse = JSON.parse(response.body)
 
-        session[:last_updated] ||= Time.now
+          #Updates db and session to new tokens
+          token_new = Token.find_by(user_id: current_user.id).update(access_token: parsedResponse['access_token'], refresh_token: parsedResponse['refresh_token'], api_server: parsedResponse['api_server'], last_updated: Time.now, user_id: current_user.id)
+
+          session[:last_updated] ||= Time.now
+
+          rescue => e
+            #Remove the old token from db
+            Token.find_by(user_id: current_user.id).destroy
+
+            #Sends a flash message telling the user to reconnect the questrade account
+            flash.now[:alert] = "Please reconnect your questrade account."
+
+        end
       end
     end
   end
@@ -51,5 +62,4 @@ class ApplicationController < ActionController::Base
       redirect_to root_path, alert: "You need to sign in or sign up before continuing."
     end
   end
-
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,17 +8,27 @@ class HomeController < ApplicationController
     if !Token.find_by(user_id: current_user.id).nil?
       session[:last_updated] = Token.find_by(user_id: current_user.id).last_updated
 
-      #updates the access and refresh token if the difference between current time and last updated is more than 30 minutes.
-      if Time.now - session[:last_updated] > 1800
-        #Sends a post request once code is recieved to get access and refresh token
-        response = Faraday.get("https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=#{Token.find_by(user_id: current_user.id).refresh_token}")
-        #Parses the response to json
-        parsedResponse = JSON.parse(response.body)
 
-        #Updates db and session to new tokens
-        token_new = Token.find_by(user_id: current_user.id).update(access_token: parsedResponse['access_token'], refresh_token: parsedResponse['refresh_token'], api_server: parsedResponse['api_server'], last_updated: Time.now, user_id: current_user.id)
+        #updates the access and refresh token if the difference between current time and last updated is more than 30 minutes.
+        if Time.now - session[:last_updated] > 1800
+          begin
+            #Sends a post request once code is recieved to get access and refresh token
+            response = Faraday.get("https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=#{Token.find_by(user_id: current_user.id).refresh_token}")
 
-        session[:last_updated] ||= Time.now
+            #Parses the response to json
+            parsedResponse = JSON.parse(response.body)
+
+            #Updates db and session to new tokens
+            token_new = Token.find_by(user_id: current_user.id).update(access_token: parsedResponse['access_token'], refresh_token: parsedResponse['refresh_token'], api_server: parsedResponse['api_server'], last_updated: Time.now, user_id: current_user.id)
+
+            session[:last_updated] ||= Time.now
+            rescue => e
+            #Remove the old token from db
+            Token.find_by(user_id: current_user.id).destroy
+
+            #Sends a flash message telling the user to reconnect the questrade account
+            flash.now[:alert] = "Please reconnect your questrade account."
+          end
       end
     end
    end
